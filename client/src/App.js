65c7d27c08d4719e049fcff6c9e0058fe77387d8
kreker36barabashka <tackabiog@hotmail.com>
2025-08-31 04:00:14 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Trang Web Động Cao</h1>
+      <p>Ứng dụng web để quản lý lưu lượng truy cập.</p>
+    </div>
+  );
+}
+
+export default App;
